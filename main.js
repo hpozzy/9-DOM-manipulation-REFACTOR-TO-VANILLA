fb71930dc356c43c33e30f10775510b78d8a760f
Punchcode <punchcode@Punchcodes-MacBook-Pro.local>
2020-09-06 21:21:44 -0700

    newest

--- main.js
+++ main.js
@@ -1,9 +1,8 @@
 let acc = document.getElementsByClassName("accordion")
-var i
-
+let i
 for (i = 0; i < acc.length; i++) {
   acc[i].addEventListener("click", function () {
-    var panel = this.nextElementSibling
+    let panel = this.nextElementSibling
     if (panel.style.maxHeight) {
       panel.style.maxHeight = null
     } else {
